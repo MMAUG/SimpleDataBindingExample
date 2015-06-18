e59ae8c48f5ad131cf453e627bcadbbfc59e8bb8
Ye Lin Aung <me@yelinaung.com>
2015-06-18 14:14:55 +0630

    init

---
+++ app/src/main/java/org/mmaug/simpledatabinding/MainActivity.java
@@ -0,0 +1,38 @@
+package org.mmaug.simpledatabinding;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import org.mmaug.simpledatabinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    User user = new User("MeowYLA", "16", "Yangon, Myanmar");
+    binding.setUser(user);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
---
+++ app/src/main/java/org/mmaug/simpledatabinding/User.java
@@ -0,0 +1,16 @@
+package org.mmaug.simpledatabinding;
+
+/**
+ * Created by Ye Lin Aung on 15/06/15.
+ */
+public class User {
+  public String name;
+  public String age;
+  public String address;
+
+  public User(String name, String age, String address) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+}
